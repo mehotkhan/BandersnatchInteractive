9981da7b6e7fd6cfa2ff272933b9150346127a86
EL-R1 <erwan.leblanc.pro@gmail.com>
2023-06-18 23:04:05 +0200

    feat: add french choices translation file

---
+++ assets/choices/fr.js
@@ -0,0 +1,678 @@
+fr={
+    "2G": {
+        "1R": "PHAEDRA",
+        "1S": "THE BERMUDA TRIANGLE"
+    },
+    "3AF2": {
+        "7A": "LE LOGO NETFLIX 'N'",
+        "nsg-WhiteBearChoice": "GLYPHE 'OURS BLANC'"
+    },
+    "3AF1": {
+        "nsg-LettersPACSChoice": "PACS",
+        "nsg-WhiteBearChoice": "GLYPHE 'OURS BLANC'"
+    },
+    "SS20": {
+        "R5": "VISITE DR HAYNES",
+        "3Nx": "REVENIR"
+    },
+    "SS21": {
+        "R6": "'PROGRAMMER ET COMMANDER'",
+        "5H": "TUER PAPA"
+    },
+    "SS22": {
+        "nsg-WhoThereChoice": "QUI EST LÀ ?",
+        "5H": "TUER PAPA"
+    },
+    "2Bp2": {
+        "No": "NON",
+        "No": "NON"
+    },
+    "3Xcd": {
+        "nsg-ThrowThemChoice": "LES JETER DANS LA POUBELLE",
+        "nsg-FlushThemChoice3X": "LES JETER DANS LES TOILETTES"
+    },
+    "2Bp1": {
+        "No": "NON",
+        "3B": "LANCER LE THÉ SUR L'ORDINATEUR",
+        "3C": "CRIER SUR PAPA"
+    },
+    "Z62f": {
+        "7A": "LE LOGO NETFLIX 'N'",
+        "nsg-WhiteBearChoice": "GLYPHE 'OURS BLANC'"
+    },
+    "3Vx": {
+        "3Y": "DÉTRUIRE L'ORDINATEUR",
+        "3Z": "FRAPPER LE BUREAU"
+    },
+    "S44A": {
+        "R3": "PARLE DE MAMAN",
+        "0cr1": "ALLER AUX CRÉDITS"
+    },
+    "Z62e": {
+        "7A": "LE LOGO NETFLIX 'N'",
+        "nsg-WhiteBearChoice": "GLYPHE 'OURS BLANC'"
+    },
+    "S44B": {
+        "R3": "PARLE DE MAMAN",
+        "0cr1": "ALLER AUX CRÉDITS"
+    },
+    "Z62d": {
+        "7A": "LE LOGO NETFLIX 'N'",
+        "nsg-WhiteBearChoice": "GLYPHE 'OURS BLANC'"
+    },
+    "7D": {
+        "Yeah": "OUAIS",
+        "FYeah": "PUTAIN OUAIS",
+        "7L": "SAUT PAR LA FENÊTRE",
+        "7H": "LA COMBATTRE"
+    },
+    "Z62c": {
+        "7A": "LE LOGO NETFLIX 'N'",
+        "nsg-WhiteBearChoice": "GLYPHE 'OURS BLANC'"
+    },
+    "3Vff": {
+        "3Y": "DÉTRUIRE L'ORDINATEUR",
+        "3Z": "FRAPPER LE BUREAU"
+    },
+    "1H": {
+        "nsg-8AChoice": "ACCEPTER",
+        "nsg-1Qnw-1Qtt": "REFUSER"
+    },
+    "ZQ": {
+        "nsg-FamilyPhotoChoice": "PRENDRE LA PHOTO DE FAMILLE",
+        "nsg-BookChoice": "PRENDRE LE LIVRE"
+    },
+    "1G": {
+        "nsg-8AChoice": "ACCEPTER",
+        "nsg-1Qnw-1Qtt": "REFUSER"
+    },
+    "ZP": {
+        "3Y": "DÉTRUIRE L'ORDINATEUR",
+        "3Z": "FRAPPER LE BUREAU"
+    },
+    "5AG2": {
+        "5AH": "LE LAISSER PARTIR",
+        "5AJ": "LE TUER"
+    },
+    "SS16": {
+        "R3": "PARLE DE MAMAN",
+        "3Vx": "REVENIR"
+    },
+    "SS17": {
+        "R4": "SUIVRE COLIN",
+        "3Vx": "REVENIR"
+    },
+    "1S": {
+        "3B": "LANCER LE THÉ SUR L'ORDINATEUR",
+        "3C": "CRIER SUR PAPA"
+    },
+    "SS18": {
+        "3Vx": "REVENIR",
+        "3Vx": "REVENIR"
+    },
+    "1R": {
+        "3B": "LANCER LE THÉ SUR L'ORDINATEUR",
+        "3C": "CRIER SUR PAPA"
+    },
+    "SS19": {
+        "R3": "PARLE DE MAMAN",
+        "3Nx": "REVENIR"
+    },
+    "Z62b": {
+        "7A": "LE LOGO NETFLIX 'N'",
+        "nsg-WhiteBearChoice": "GLYPHE 'OURS BLANC'"
+    },
+    "Z62a": {
+        "7A": "LE LOGO NETFLIX 'N'",
+        "nsg-WhiteBearChoice": "GLYPHE 'OURS BLANC'"
+    },
+    "2Bt1": {
+        "No": "NON",
+        "3B": "LANCER LE THÉ SUR L'ORDINATEUR",
+        "3C": "CRIER SUR PAPA"
+    },
+    "3Vfs": {
+        "3Y": "DÉTRUIRE L'ORDINATEUR",
+        "3Z": "FRAPPER LE BUREAU"
+    },
+    "SS10": {
+        "1PA": "REVENIR",
+        "1PA": "REVENIR"
+    },
+    "SS11": {
+        "nsg-SS11-0": "TRAVAILLER CHEZ TUCKERSOFT ?",
+        "3Ax": "REVENIR"
+    },
+    "SS12": {
+        "R3": "PARLE DE MAMAN",
+        "3Ax": "REVENIR"
+    },
+    "SS13": {
+        "3Ax": "REVENIR",
+        "3Ax": "REVENIR"
+    },
+    "SS14": {
+        "R3": "PARLE DE MAMAN",
+        "nsg-SS14-1": "REVENIR"
+    },
+    "SS15": {
+        "R4": "SUIVRE COLIN",
+        "nsg-SS15-1": "REVENIR"
+    },
+    "3Nx": {
+        "8L": "STEFAN",
+        "3Q": "COLIN"
+    },
+    "2B": {
+        "No": "NON",
+        "1R": "PHÈDRE",
+        "1S": "LE TRIANGLE DES BERMUDES"
+    },
+    "S35B": {
+        "nsg-WhoThereChoice": "QUI EST LÀ ?",
+        "0cr1": "ALLER AUX CRÉDITS"
+    },
+    "2Gp1": {
+        "3B": "LANCER LE THÉ SUR L'ORDINATEUR",
+        "3C": "CRIER SUR PAPA"
+    },
+    "3AH2": {
+        "7A": "LE LOGO NETFLIX 'N'",
+        "nsg-WhiteBearChoice": "GLYPHE 'OURS BLANC'"
+    },
+    "3Wbs": {
+        "3Y": "DÉTRUIRE L'ORDINATEUR",
+        "3Z": "FRAPPER LE BUREAU"
+    },
+    "1HB": {
+        "nsg-8AChoice": "ACCEPTER",
+        "nsg-1Q_rewatch": "REFUSER"
+    },
+    "3AH1": {
+        "nsg-LettersPACSChoice": "PACS",
+        "nsg-WhiteBearChoice": "GLYPHE 'OURS BLANC'"
+    },
+    "S59A": {
+        "nsg-S59A-0": "COUPER OU ENTERRER ?",
+        "0cr1": "ALLER AUX CRÉDITS"
+    },
+    "S59B": {
+        "nsg-S59B-0": "COUPER OU ENTERRER ?",
+        "0cr1": "ALLER AUX CRÉDITS"
+    },
+    "S35A": {
+        "nsg-WhoThereChoice": "QUI EST LÀ ?",
+        "0cr1": "ALLER AUX CRÉDITS"
+    },
+    "5A": {
+        "5H": "TUER PAPA",
+        "5G": "DÉSACTIVER"
+    },
+    "5QA": {
+        "5UA": "OUI",
+        "5TA": "NON"
+    },
+    "1QA": {
+        "3B": "LANCER LE THÉ SUR L'ORDINATEUR",
+        "3C": "CRIER SUR PAPA"
+    },
+    "8JB1": {
+        "nsg-LettersPACSChoice": "PACS",
+        "nsg-WhiteBearChoice": "GLYPHE 'OURS BLANC'"
+    },
+    "S34A": {
+        "R4": "SUIVRE COLIN",
+        "0cr1": "ALLER AUX CRÉDITS"
+    },
+    "3N": {
+        "8L": "STEFAN",
+        "3Q": "COLIN"
+    },
+    "8JB2": {
+        "7A": "LE LOGO NETFLIX 'N'",
+        "nsg-WhiteBearChoice": "GLYPHE 'OURS BLANC'"
+    },
+    "S34B": {
+        "R4": "SUIVRE COLIN",
+        "0cr1": "ALLER AUX CRÉDITS"
+    },
+    "1Qtt": {
+        "nsg-2BChoice": "OUI",
+        "2GA": "NON"
+    },
+    "3M": {
+        "3N": "OUI",
+        "3P": "NON"
+    },
+    "3L": {
+        "nsg-VisitHaynesChoice": "VISITE DR HAYNES",
+        "nsg-FollowColinChoice": "SUIVRE COLIN"
+    },
+    "3J": {
+        "nsg-VisitHaynesChoice": "VISITE DR HAYNES",
+        "nsg-FollowColinChoice": "SUIVRE COLIN"
+    },
+    "5AD": {
+        "5AF": "AUCUNE IDÉE",
+        "5AE": "IL A SAUTÉ"
+    },
+    "3R": {
+        "3U": "SE RONGER LES ONGLES",
+        "3T": "SE TOUCHER L'OREILLE"
+    },
+    "3P": {
+        "8L": "STEFAN",
+        "3Q": "COLIN"
+    },
+    "3Z": {
+        "nsg-FamilyPhotoChoice": "PRENDRE LA PHOTO DE FAMILLE",
+        "nsg-BookChoice": "PRENDRE LE LIVRE"
+    },
+    "3X": {
+        "8B_Variant2": "LES PRENDRE",
+        "nsg-FlushThemChoice3X": "LES JETER DANS LES TOILETTES"
+    },
+    "3VA": {
+        "3Y": "DÉTRUIRE L'ORDINATEUR",
+        "3Z": "FRAPPER LE BUREAU"
+    },
+    "3Wbf": {
+        "3Y": "DÉTRUIRE L'ORDINATEUR",
+        "3Z": "FRAPPER LE BUREAU"
+    },
+    "ZK1": {
+        "8J": "JOUET",
+        "nsg-PAXChoice": "PAX"
+    },
+    "ZK2": {
+        "nsg-PAXChoice": "PAX",
+        "nsg-PACStudyChoice": "PAC"
+    },
+    "5AG": {
+        "5AH": "LE LAISSER PARTIR",
+        "5AJ": "LE TUER"
+    },
+    "ZK3": {
+        "8J": "JOUET",
+        "nsg-PACStudyChoice": "PAC"
+    },
+    "3VB": {
+        "nsg-FamilyPhotoChoice": "PRENDRE LA PHOTO DE FAMILLE",
+        "nsg-BookChoice": "PRENDRE LE LIVRE"
+    },
+    "3AB2": {
+        "7A": "LE LOGO NETFLIX 'N'",
+        "nsg-WhiteBearChoice": "GLYPHE 'OURS BLANC'"
+    },
+    "3AB1": {
+        "nsg-LettersPACSChoice": "PACS",
+        "nsg-WhiteBearChoice": "GLYPHE 'OURS BLANC'"
+    },
+    "2Bt2": {
+        "No": "NON",
+        "No": "NON"
+    },
+    "3AJ2": {
+        "7A": "LE LOGO NETFLIX 'N'",
+        "nsg-WhiteBearChoice": "GLYPHE 'OURS BLANC'"
+    },
+    "3AJ1": {
+        "nsg-LettersPACSChoice": "PACS",
+        "nsg-WhiteBearChoice": "GLYPHE 'OURS BLANC'"
+    },
+    "S37A": {
+        "R2": "PRENDRE LAPIN DE PAPA",
+        "0cr1": "ALLER AUX CRÉDITS"
+    },
+    "S37B": {
+        "R2": "PRENDRE LAPIN DE PAPA",
+        "0cr1": "ALLER AUX CRÉDITS"
+    },
+    "5AD2": {
+        "5AF": "AUCUNE IDÉE",
+        "5AE": "IL A SAUTÉ"
+    },
+    "3Xxc": {
+        "8B": "LES JETER DANS LA POUBELLE",
+        "nsg-FlushThemChoice3X": "LES JETER DANS LES TOILETTES"
+    },
+    "7B": {
+        "7C": "DITES-LUI PLUS",
+        "7D": "ARRÊTER LA CONVERSATION"
+    },
+    "7A": {
+        "7B": "DITES-LUI PLUS",
+        "7B2": "ESSAYER D'EXPLIQUER"
+    },
+    "3Xxa": {
+        "8B_Variant2": "LES PRENDRE",
+        "nsg-FlushThemChoice3X": "LES JETER DANS LES TOILETTES"
+    },
+    "3Ax": {
+        "3B": "LANCER LE THÉ SUR L'ORDINATEUR",
+        "3C": "CRIER SUR PAPA"
+    },
+    "3AK1": {
+        "nsg-LettersPACSChoice": "PACS",
+        "nsg-WhiteBearChoice": "GLYPHE 'OURS BLANC'"
+    },
+    "7H": {
+        "7K": "COUP DE KARATÉ A PAPA",
+        "7J": "COUP DE PIEDS DANS LES BURNES"
+    },
+    "5Q": {
+        "5U": "OUI",
+        "5T": "NON"
+    },
+    "SS7": {
+        "R1": "PRENDRE PHOTO",
+        "5H": "TUER PAPA"
+    },
+    "SS8": {
+        "R2": "PRENDRE LAPIN DE PAPA",
+        "5H": "TUER PAPA"
+    },
+    "SS9": {
+        "R4": "SUIVRE COLIN",
+        "5H": "TUER PAPA"
+    },
+    "SS3": {
+        "R2": "PRENDRE LAPIN DE PAPA",
+        "nsg-WhoThereChoice": "QUI EST LÀ ?"
+    },
+    "SS4": {
+        "R4": "SUIVRE COLIN",
+        "nsg-WhoThereChoice": "QUI EST LÀ ?"
+    },
+    "SS5": {
+        "nsg-WhoThereChoice": "QUI EST LÀ ?"
+    },
+    "SS6": {
+        "R3": "PARLE DE MAMAN",
+        "5H": "TUER PAPA"
+    },
+    "1GB": {
+        "nsg-8AChoice": "ACCEPTER",
+        "nsg-1Q_rewatch": "REFUSER"
+    },
+    "SS1": {
+        "R3": "PARLE DE MAMAN",
+        "R4": "SUIVRE COLIN"
+    },
+    "1GA": {
+        "nsg-8AChoice": "ACCEPTER",
+        "nsg-1Q_rewatch": "REFUSER"
+    },
+    "5V": {
+        "nsg-5V-0": "ENTERRER LE CORPS",
+        "nsg-5V-1": "COUPER LE CORPS"
+    },
+    "SS2": {
+        "R1": "PRENDRE PHOTO",
+        "nsg-WhoThereChoice": "QUI EST LÀ ?"
+    },
+    "S36A": {
+        "R1": "PRENDRE PHOTO",
+        "0cr1": "ALLER AUX CRÉDITS"
+    },
+    "S36B": {
+        "R1": "PRENDRE PHOTO",
+        "0cr1": "ALLER AUX CRÉDITS"
+    },
+    "SS52": {
+        "nsg-SS52-0": "COUPER OU ENTERRER ?",
+        "0cr2": "ALLER AUX CRÉDITS"
+    },
+    "SS53": {
+        "nsg-SS53-0": "COUPER OU ENTERRER ?",
+        "0cr2": "ALLER AUX CRÉDITS"
+    },
+    "SS54": {
+        "nsg-WhoThereChoice-SS54": "QUI EST LÀ ?",
+        "0cr2": "ALLER AUX CRÉDITS"
+    },
+    "SS55": {
+        "nsg-SS55-0": "PRENDRE PHOTO",
+        "0cr2": "ALLER AUX CRÉDITS"
+    },
+    "1HA": {
+        "nsg-8AChoice": "ACCEPTER",
+        "nsg-1Q_rewatch": "REFUSER"
+    },
+    "SS56": {
+        "nsg-SS56-0": "PRENDRE LAPIN DE PAPA",
+        "0cr2": "ALLER AUX CRÉDITS"
+    },
+    "3Vbs": {
+        "3Y": "DÉTRUIRE L'ORDINATEUR",
+        "3Z": "FRAPPER LE BUREAU"
+    },
+    "SS57": {
+        "R6": "'PROGRAMMER ET COMMANDER'",
+        "0cr2": "ALLER AUX CRÉDITS"
+    },
+    "3Vbf": {
+        "3Y": "DÉTRUIRE L'ORDINATEUR",
+        "3Z": "FRAPPER LE BUREAU"
+    },
+    "1PA": {
+        "nsg-8AChoice": "ACCEPTER",
+        "nsg-1Q_rewatch": "REFUSER"
+    },
+    "1Qnw": {
+        "nsg-2BChoice": "OUI",
+        "2GA": "NON"
+    },
+    "Z4cd": {
+        "3Y": "DÉTRUIRE L'ORDINATEUR",
+        "3Z": "FRAPPER LE BUREAU"
+    },
+    "SS50": {
+        "nsg-SS50-0": "PARLE DE MAMAN",
+        "0cr2": "ALLER AUX CRÉDITS"
+    },
+    "SS51": {
+        "nsg-SS51-0": "SUIVRE COLIN",
+        "0cr2": "ALLER AUX CRÉDITS"
+    },
+    "Z61e": {
+        "nsg-LettersPACSChoice": "PACS",
+        "nsg-WhiteBearChoice": "GLYPHE 'OURS BLANC'"
+    },
+    "3AD3": {
+        "8J": "JOUET",
+        "nsg-PACStudyChoice": "PAC"
+    },
+    "Z61d": {
+        "nsg-LettersPACSChoice": "PACS",
+        "nsg-WhiteBearChoice": "GLYPHE 'OURS BLANC'"
+    },
+    "Z4": {
+        "3Y": "DÉTRUIRE L'ORDINATEUR",
+        "3Z": "FRAPPER LE BUREAU"
+    },
+    "Z61c": {
+        "nsg-LettersPACSChoice": "PACS",
+        "nsg-WhiteBearChoice": "GLYPHE 'OURS BLANC'"
+    },
+    "Z61b": {
+        "nsg-LettersPACSChoice": "PACS",
+        "nsg-WhiteBearChoice": "GLYPHE 'OURS BLANC'"
+    },
+    "Z1": {
+        "3B": "LANCER LE THÉ SUR L'ORDINATEUR",
+        "3C": "CRIER SUR PAPA"
+    },
+    "Z61f": {
+        "nsg-LettersPACSChoice": "PACS",
+        "nsg-WhiteBearChoice": "GLYPHE 'OURS BLANC'"
+    },
+    "Z61a": {
+        "nsg-LettersPACSChoice": "PACS",
+        "nsg-WhiteBearChoice": "GLYPHE 'OURS BLANC'"
+    },
+    "3AD2": {
+        "nsg-PAXChoice": "PAX",
+        "nsg-PACStudyChoice": "PAC"
+    },
+    "3AD1": {
+        "8J": "JOUET",
+        "nsg-PAXChoice": "PAX"
+    },
+    "ZE": {
+        "nsg-FamilyPhotoChoice": "PRENDRE LA PHOTO DE FAMILLE",
+        "nsg-BookChoice": "PRENDRE LE LIVRE"
+    },
+    "ZD": {
+        "nsg-FamilyPhotoChoice": "PRENDRE LA PHOTO DE FAMILLE",
+        "nsg-BookChoice": "PRENDRE LE LIVRE"
+    },
+    "4C2": {
+        "7A": "LE LOGO NETFLIX 'N'",
+        "nsg-WhiteBearChoice": "GLYPHE 'OURS BLANC'"
+    },
+    "ZB": {
+        "nsg-2BChoice": "OUI",
+        "2GA": "NON"
+    },
+    "1E": {
+        "1H": "LES THOMPSON TWINS",
+        "1G": "NOW 2"
+    },
+    "3Xac": {
+        "nsg-ThrowThemChoice": "LES JETER DANS LA POUBELLE",
+        "nsg-FlushThemChoice3X": "LES JETER DANS LES TOILETTES"
+    },
+    "1D": {
+        "1H": "LES THOMPSON TWINS",
+        "1G": "NOW 2"
+    },
+    "3Xa": {
+        "8B_Variant2": "LES PRENDRE",
+        "nsg-FlushThemChoice3X": "LES JETER DANS LES TOILETTES"
+    },
+    "ZK": {
+        "nsg-JFDChoice": "JFD",
+        "nsg-PAXChoice": "PAX"
+    },
+    "1A": {
+        "1E": "SUGAR PUFFS",
+        "1D": "FROSTIES"
+    },
+    "4C1": {
+        "nsg-LettersPACSChoice": "PACS",
+        "nsg-WhiteBearChoice": "GLYPHE 'OURS BLANC'"
+    },
+    "ZJ": {
+        "nsg-FamilyPhotoChoice": "PRENDRE LA PHOTO DE FAMILLE",
+        "nsg-BookChoice": "PRENDRE LE LIVRE"
+    },
+    "ZH": {
+        "3Y": "DÉTRUIRE L'ORDINATEUR",
+        "3Z": "FRAPPER LE BUREAU"
+    },
+    "ZG": {
+        "8L": "STEFAN",
+        "3Q": "COLIN"
+    },
+    "3AD": {
+        "nsg-JFDChoice": "JFD",
+        "nsg-PAXChoice": "PAX"
+    },
+    "3AC2": {
+        "7A": "LE LOGO NETFLIX 'N'",
+        "nsg-WhiteBearChoice": "GLYPHE 'OURS BLANC'"
+    },
+    "3AC": {
+        "nsg-ThrowTeaChoice": "LANCER LE THÉ SUR L'ORDINATEUR",
+        "nsg-DestroyComputerChoice": "DÉTRUIRE L'ORDINATEUR"
+    },
+    "3AB": {
+        "nsg-ThrowTeaChoice": "LANCER LE THÉ SUR L'ORDINATEUR",
+        "nsg-DestroyComputerChoice": "DÉTRUIRE L'ORDINATEUR"
+    },
+    "ZHw6": {
+        "nsg-FamilyPhotoChoice": "PRENDRE LA PHOTO DE FAMILLE",
+        "nsg-BookChoice": "PRENDRE LE LIVRE"
+    },
+    "3AH": {
+        "nsg-ThrowTeaChoice": "LANCER LE THÉ SUR L'ORDINATEUR",
+        "nsg-DestroyComputerChoice": "DÉTRUIRE L'ORDINATEUR"
+    },
+    "3Wff": {
+        "3Y": "DÉTRUIRE L'ORDINATEUR",
+        "3Z": "FRAPPER LE BUREAU"
+    },
+    "3AF": {
+        "nsg-ThrowTeaChoice": "LANCER LE THÉ SUR L'ORDINATEUR",
+        "nsg-DestroyComputerChoice": "DÉTRUIRE L'ORDINATEUR"
+    },
+    "3AK2": {
+        "7A": "LE LOGO NETFLIX 'N'",
+        "nsg-WhiteBearChoice": "GLYPHE 'OURS BLANC'"
+    },
+    "2GA": {
+        "nsg-2BChoice": "OUI",
+        "nsg-2GChoice": "NON"
+    },
+    "S38A": {
+        "R6": "'PROGRAMMER ET COMMANDER'",
+        "0cr1": "ALLER AUX CRÉDITS"
+    },
+    "S38B": {
+        "R6": "'PROGRAMMER ET COMMANDER'",
+        "0cr1": "ALLER AUX CRÉDITS"
+    },
+    "3AC1": {
+        "nsg-LettersPACSChoice": "PACS",
+        "nsg-WhiteBearChoice": "GLYPHE 'OURS BLANC'"
+    },
+    "2Gt1": {
+        "3B": "LANCER LE THÉ SUR L'ORDINATEUR",
+        "3C": "CRIER SUR PAPA"
+    },
+    "3AL2": {
+        "7A": "LE LOGO NETFLIX 'N'",
+        "nsg-WhiteBearChoice": "GLYPHE 'OURS BLANC'"
+    },
+    "3Wfs": {
+        "3Y": "DÉTRUIRE L'ORDINATEUR",
+        "3Z": "FRAPPER LE BUREAU"
+    },
+    "3AL1": {
+        "nsg-LettersPACSChoice": "PACS",
+        "nsg-WhiteBearChoice": "GLYPHE 'OURS BLANC'"
+    },
+    "3AL": {
+        "nsg-ThrowTeaChoice": "LANCER LE THÉ SUR L'ORDINATEUR",
+        "nsg-DestroyComputerChoice": "DÉTRUIRE L'ORDINATEUR"
+    },
+    "3AK": {
+        "nsg-ThrowTeaChoice": "LANCER LE THÉ SUR L'ORDINATEUR",
+        "nsg-DestroyComputerChoice": "DÉTRUIRE L'ORDINATEUR"
+    },
+    "8K": {
+        "nsg-8K-0": "LANCER LE THÉ SUR L'ORDINATEUR",
+        "nsg-8K-1": "DÉTRUIRE L'ORDINATEUR"
+    },
+    "3AJ": {
+        "nsg-ThrowTeaChoice": "LANCER LE THÉ SUR L'ORDINATEUR",
+        "nsg-DestroyComputerChoice": "DÉTRUIRE L'ORDINATEUR"
+    },
+    "8J": {
+        "8JA": "OUI",
+        "nsg-8J-1": "NON"
+    },
+    "4D2": {
+        "7A": "LE LOGO NETFLIX 'N'",
+        "nsg-WhiteBearChoice": "GLYPHE 'OURS BLANC'"
+    },
+    "4D1": {
+        "nsg-LettersPACSChoice": "PACS",
+        "nsg-WhiteBearChoice": "GLYPHE 'OURS BLANC'"
+    },
+    "5VA": {
+        "nsg-5V-0": "ENTERRER LE CORPS",
+        "nsg-5V-1": "COUPER LE CORPS"
+    }
+}
