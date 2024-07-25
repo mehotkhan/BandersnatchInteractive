73dc8bd1d6cc96a43b35fea0e6398126756fa0f2
Ali Zemani <mehot1@gmail.com>
2024-07-25 19:50:15 +0330

    Init Electrin-Vite

---
+++ electron.vite.config.ts
@@ -0,0 +1,18 @@
+import { resolve } from 'path'
+import { defineConfig, externalizeDepsPlugin, bytecodePlugin } from 'electron-vite'
+
+export default defineConfig({
+  main: {
+    plugins: [externalizeDepsPlugin(), bytecodePlugin()]
+  },
+  preload: {
+    plugins: [externalizeDepsPlugin(), bytecodePlugin()]
+  },
+  renderer: {
+    resolve: {
+      alias: {
+        '@renderer': resolve('src/renderer/src')
+      }
+    },
+  }
+})
